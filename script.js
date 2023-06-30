9b5cec4eab512110bb13ff2d742165572ebdaf46
basnetrajpradip <basnetrajpradip@gmail.com>
2023-06-30 20:35:57 +0545

    Polish the UX of the game

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function GameBoard() {
 }
 
 //module which controls the overall gameplay
-function GameController(player1 = "Ram", marker1 = "X", player2 = "Hari", marker2 = "O") {
+function GameController(player1 = "Player 1", marker1 = "X", player2 = "Player 2", marker2 = "O") {
   const board = GameBoard();
 
   //array of objects which stores players name and the marker they choose to play with
@@ -43,7 +43,7 @@ function GameController(player1 = "Ram", marker1 = "X", player2 = "Hari", marker
   };
 
   //variable that stores the record of which player turn it is
-  let activePlayer = players[0];
+  let activePlayer = players[1];
 
   //function to switch the turn of the player
   const switchPlayerTurn = () => {
@@ -118,6 +118,7 @@ function GameController(player1 = "Ram", marker1 = "X", player2 = "Hari", marker
     playRound,
     clearBoard,
     getBoard: board.getBoard,
+    players,
   };
 }
 
@@ -125,12 +126,47 @@ const ScreenController = (() => {
   const gameMode = document.querySelector("#vsPlayerBtn");
   const preGameUI = document.querySelector(".pre-game-interface");
   const gameUI = document.querySelector(".game-interface");
+  const playerUI = document.querySelector(".player-selection-interface");
   const returnToPreGame = document.querySelector("#return");
   const playAgain = document.querySelector("#replay");
   const winnerDisplay = document.querySelector(".game-result");
+  const playbtn = document.querySelector("#play");
+  const inputs = document.querySelectorAll("input");
+  let activePlayer1 = document.querySelector(".player1");
+  let activePlayer2 = document.querySelector(".player2");
 
+  let player1 = "",
+    marker1 = "",
+    player2 = "",
+    marker2 = "";
+
+  let game, board, activePlayer;
   gameMode.addEventListener("click", (e) => {
     preGameUI.classList.add("pre-game-display");
+    playerUI.classList.remove("player-selection-display");
+  });
+
+  playbtn.addEventListener("click", (e) => {
+    player1 = inputs[0].value;
+    player2 = inputs[3].value;
+    marker1 = inputs[1].checked ? inputs[1].value : inputs[2].value;
+    marker2 = inputs[4].checked ? inputs[4].value : inputs[5].value;
+
+    if (player1 !== "" || player2 !== "") {
+      activePlayer1.textContent = player1;
+      activePlayer2.textContent = player2;
+    } else {
+      activePlayer1.textContent = "Player 1";
+      activePlayer2.textContent = "Player 2";
+    }
+
+    if (player1 === "" || player2 === "") {
+      game = GameController();
+    } else game = GameController(player1, marker1, player2, marker2);
+
+    board = game.getBoard();
+
+    playerUI.classList.add("player-selection-display");
     gameUI.classList.remove("game-display");
   });
 
@@ -140,9 +176,6 @@ const ScreenController = (() => {
     clearScreen(e);
   });
 
-  const game = GameController();
-  const board = game.getBoard();
-  const activePlayer = game.getActivePlayer();
   const columns = document.querySelectorAll("td");
 
   function clickHandler(event) {
@@ -155,12 +188,16 @@ const ScreenController = (() => {
 
       this.textContent = game.getActivePlayer().marker;
 
+      activePlayer1.classList.toggle("active");
+      activePlayer2.classList.toggle("active");
+
       let winner = game.checkWinner();
-      console.log(winner);
 
       if (winner != undefined && winner.win) {
         if (winner.player !== "Draw") {
           winnerDisplay.textContent = `${winner.player} Wins the Game`;
+          activePlayer1.classList.remove("active");
+          activePlayer2.classList.remove("active");
         }
         if (winner.player === "Draw") {
           winnerDisplay.textContent = `Match is Draw`;
@@ -175,6 +212,7 @@ const ScreenController = (() => {
   function clearScreen(event) {
     game.clearBoard(board);
     winnerDisplay.textContent = "";
+    activePlayer1.classList.add("active");
     columns.forEach((column) => {
       column.textContent = "";
     });
